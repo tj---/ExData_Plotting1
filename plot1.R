6ab579c00fd144e8ca339f9d597bee89d39d6c0b
Trilok Jain <trilokj@gmail.com>
2015-06-04 01:16:41 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,5 @@
+source("util.R")
+data <- load_data()	# Load the relevant data from the file
+png(file = "plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
